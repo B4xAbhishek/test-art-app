a8859e5acc1a2913e7425506a86fcc6eca250866
sopspops <adorable_sj@hotmail.com>
2022-02-02 10:33:01 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,23 @@
-import React, { useEffect, useLayoutEffect, useState, useRef } from "react"
-import rough from 'roughjs/bundled/rough.esm'
-import { getStroke } from "perfect-freehand"
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import getStroke from "perfect-freehand";
 
-const generator = rough.generator();
-
-const createElement = (id, x1, y1, x2, y2, type) => { 
+const createElement = (id, x1, y1, x2, y2, type) => {
   switch (type) {
     case "line":
     case "rectangle":
-      const roughElement = 
-        type === "line" 
-          ? generator.line(x1, y1, x2, y2)
-          : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
-       return { id, x1, y1, x2, y2, type, roughElement } 
+      return { id, x1, y1, x2, y2, type };
     case "pencil":
-      return { id, type, points: [{ x: x1, y: y1}] };
+      return { id, type, points: [{ x: x1, y: y1 }] };
     case "text":
-      return {id, type, x1, y1, x2, y2, text: "" };
+      return { id, type, x1, y1, x2, y2, text: "" };
     default:
-      throw new Error(`Type not recognized: ${type}`)
+      throw new Error(`Type not recognised: ${type}`);
   }
-}
+};
 
 const nearPoint = (x, y, x1, y1, name) => {
-  return Math.abs(x - x1) < 5 && Math.abs (y - y1) < 5 ? name : null;
-}
+  return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5 ? name : null;
+};
 
 const onLine = (x1, y1, x2, y2, x, y, maxDistance = 1) => {
   const a = { x: x1, y: y1 };
@@ -32,13 +25,13 @@ const onLine = (x1, y1, x2, y2, x, y, maxDistance = 1) => {
   const c = { x, y };
   const offset = distance(a, b) - (distance(a, c) + distance(b, c));
   return Math.abs(offset) < maxDistance ? "inside" : null;
-}
+};
 
 const positionWithinElement = (x, y, element) => {
   const { type, x1, x2, y1, y2 } = element;
   switch (type) {
     case "line":
-      const on = onLine(x1, y1, x2, y2, x, y, 5);
+      const on = onLine(x1, y1, x2, y2, x, y);
       const start = nearPoint(x, y, x1, y1, "start");
       const end = nearPoint(x, y, x2, y2, "end");
       return start || end || on;
@@ -48,28 +41,28 @@ const positionWithinElement = (x, y, element) => {
       const bottomLeft = nearPoint(x, y, x1, y2, "bl");
       const bottomRight = nearPoint(x, y, x2, y2, "br");
       const inside = x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : null;
-      return topLeft || topRight || bottomLeft || bottomRight|| inside;
+      return topLeft || topRight || bottomLeft || bottomRight || inside;
     case "pencil":
       const betweenAnyPoint = element.points.some((point, index) => {
         const nextPoint = element.points[index + 1];
         if (!nextPoint) return false;
-        return onLine(point.x, point.y, nextPoint.x, nextPoint.y, x, y) != null;
-      })
+        return onLine(point.x, point.y, nextPoint.x, nextPoint.y, x, y, 5) != null;
+      });
       return betweenAnyPoint ? "inside" : null;
     case "text":
       return x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : null;
     default:
-      throw new Error(`Type not recognized: ${type}`)
+      throw new Error(`Type not recognised: ${type}`);
   }
-}
+};
 
-const distance = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow (a.y - b.y, 2));
+const distance = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
 
 const getElementAtPosition = (x, y, elements) => {
   return elements
-  .map(element => ({...element, position: positionWithinElement(x, y, element)}))
-  .find(element => element.position !== null);
-}
+    .map(element => ({ ...element, position: positionWithinElement(x, y, element) }))
+    .find(element => element.position !== null);
+};
 
 const adjustElementCoordinates = element => {
   const { type, x1, y1, x2, y2 } = element;
@@ -78,15 +71,15 @@ const adjustElementCoordinates = element => {
     const maxX = Math.max(x1, x2);
     const minY = Math.min(y1, y2);
     const maxY = Math.max(y1, y2);
-    return { x1: minX, y1: minY, x2: maxX, y2: maxY }
+    return { x1: minX, y1: minY, x2: maxX, y2: maxY };
   } else {
     if (x1 < x2 || (x1 === x2 && y1 < y2)) {
-      return { x1, y1, x2, y2 }
+      return { x1, y1, x2, y2 };
     } else {
-      return { x1: x2, y1: y2, x2: x1, y2: y1 }
+      return { x1: x2, y1: y2, x2: x1, y2: y1 };
     }
   }
-}
+};
 
 const cursorForPosition = position => {
   switch (position) {
@@ -101,25 +94,25 @@ const cursorForPosition = position => {
     default:
       return "move";
   }
-}
+};
 
 const resizedCoordinates = (clientX, clientY, position, coordinates) => {
-  const {x1, y1, x2, y2} = coordinates;
+  const { x1, y1, x2, y2 } = coordinates;
   switch (position) {
     case "tl":
     case "start":
-      return { x1: clientX, y1: clientY, x2, y2 }
-      case "tr":
-      return { x1, y1: clientY, x2: clientX, y2 }
-      case "bl":
-      return { x1: clientX, y1, x2, y2: clientY }
-      case "br":
-      case "end":
-        return { x1, y1, x2: clientX, y2: clientY };
-      default:
-        return null;
+      return { x1: clientX, y1: clientY, x2, y2 };
+    case "tr":
+      return { x1, y1: clientY, x2: clientX, y2 };
+    case "bl":
+      return { x1: clientX, y1, x2, y2: clientY };
+    case "br":
+    case "end":
+      return { x1, y1, x2: clientX, y2: clientY };
+    default:
+      return null; //should not really get here...
   }
-}
+};
 
 const useHistory = initialState => {
   const [index, setIndex] = useState(0);
@@ -136,51 +129,57 @@ const useHistory = initialState => {
       setHistory([...updatedState, newState]);
       setIndex(prevState => prevState + 1);
     }
-  }
+  };
 
   const undo = () => index > 0 && setIndex(prevState => prevState - 1);
   const redo = () => index < history.length - 1 && setIndex(prevState => prevState + 1);
 
   return [history[index], setState, undo, redo];
-}
+};
 
-function getSvgPathFromStroke(stroke) {
-  if (!stroke.length) return ''
+const getSvgPathFromStroke = stroke => {
+  if (!stroke.length) return "";
 
   const d = stroke.reduce(
     (acc, [x0, y0], i, arr) => {
-      const [x1, y1] = arr[(i + 1) % arr.length]
-      acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2)
-      return acc
+      const [x1, y1] = arr[(i + 1) % arr.length];
+      acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2);
+      return acc;
     },
-    ['M', ...stroke[0], 'Q']
-  )
+    ["M", ...stroke[0], "Q"]
+  );
 
-  d.push('Z')
-  return d.join(' ')
-}
+  d.push("Z");
+  return d.join(" ");
+};
 
-const drawElement = (roughCanvas, context, element) => {
+const drawElement = (canvas, context, element, id, x1, y1, x2, y2) => {
   switch (element.type) {
     case "line":
+      context.beginPath();
+      context.moveTo(x1, y1);
+      context.lineTo(x2, y2);
+      context.stroke();
     case "rectangle":
-      roughCanvas.draw(element.roughElement);
+      context.beginPath();
+      context.rect(x1, y1, x2 - x1, y2 - y1);
+      context.stroke();
       break;
     case "pencil":
-      const stroke = getSvgPathFromStroke(getStroke(element.points, {size: 5}));
+      const stroke = getSvgPathFromStroke(getStroke(element.points));
       context.fill(new Path2D(stroke));
       break;
     case "text":
       context.textBaseline = "top";
-      context.font = '24px sans-serif';
+      context.font = "24px sans-serif";
       context.fillText(element.text, element.x1, element.y1);
       break;
     default:
-      throw new Error(`Type not recognized: ${element.type}`)
+      throw new Error(`Type not recognised: ${element.type}`);
   }
-}
+};
 
-const adjustmentRequired = type => ['line', 'rectangle'].includes(type);
+const adjustmentRequired = type => ["line", "rectangle"].includes(type);
 
 const IndexPage = () => {
   const [elements, setElements, undo, redo] = useHistory([]);
@@ -189,16 +188,14 @@ const IndexPage = () => {
   const [selectedElement, setSelectedElement] = useState(null);
   const textAreaRef = useRef();
 
-  useLayoutEffect( () => {
+  useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    const roughCanvas = rough.canvas(canvas);
-
     elements.forEach(element => {
       if (action === "writing" && selectedElement.id === element.id) return;
-      drawElement(roughCanvas, context, element)
+      drawElement(canvas, context, element);
     });
   }, [elements, action, selectedElement]);
 
@@ -211,11 +208,12 @@ const IndexPage = () => {
           undo();
         }
       }
-    }
+    };
+
     document.addEventListener("keydown", undoRedoFunction);
     return () => {
       document.removeEventListener("keydown", undoRedoFunction);
-    }
+    };
   }, [undo, redo]);
 
   useEffect(() => {
@@ -246,14 +244,14 @@ const IndexPage = () => {
         elementsCopy[id] = {
           ...createElement(id, x1, y1, x1 + textWidth, y1 + textHeight, type),
           text: options.text,
-        }
+        };
         break;
       default:
-        throw new Error(`Type not recognized: ${type}`)
+        throw new Error(`Type not recognised: ${type}`);
     }
 
     setElements(elementsCopy, true);
-  }
+  };
 
   const handleMouseDown = event => {
     if (action === "writing") return;
@@ -262,18 +260,18 @@ const IndexPage = () => {
     if (tool === "selection") {
       const element = getElementAtPosition(clientX, clientY, elements);
       if (element) {
-        if(element.type === "pencil") {
+        if (element.type === "pencil") {
           const xOffsets = element.points.map(point => clientX - point.x);
-          const yOffsets = element.points.map(point => clientY - point.y);;
-          setSelectedElement({...element, xOffsets, yOffsets});
+          const yOffsets = element.points.map(point => clientY - point.y);
+          setSelectedElement({ ...element, xOffsets, yOffsets });
         } else {
           const offsetX = clientX - element.x1;
           const offsetY = clientY - element.y1;
-          setSelectedElement({...element, offsetX, offsetY});
+          setSelectedElement({ ...element, offsetX, offsetY });
         }
         setElements(prevState => prevState);
 
-        if(element.position === "inside") {
+        if (element.position === "inside") {
           setAction("moving");
         } else {
           setAction("resizing");
@@ -286,35 +284,33 @@ const IndexPage = () => {
       setSelectedElement(element);
 
       setAction(tool === "text" ? "writing" : "drawing");
-      }
     }
-hbhjhj
+  };
+
   const handleMouseMove = event => {
     const { clientX, clientY } = event;
 
     if (tool === "selection") {
       const element = getElementAtPosition(clientX, clientY, elements);
-      event.target.style.cursor = element 
-      ? cursorForPosition(element.position) 
-      : "default";
+      event.target.style.cursor = element ? cursorForPosition(element.position) : "default";
     }
 
     if (action === "drawing") {
-    const index = elements.length - 1;
-    const { x1, y1 } = elements[index];
-    updateElement(index, x1, y1, clientX, clientY, tool);
+      const index = elements.length - 1;
+      const { x1, y1 } = elements[index];
+      updateElement(index, x1, y1, clientX, clientY, tool);
     } else if (action === "moving") {
       if (selectedElement.type === "pencil") {
         const newPoints = selectedElement.points.map((_, index) => ({
-            x: clientX - selectedElement.xOffsets[index],
-            y: clientY - selectedElement.yOffsets[index]
-          }));
-       const elementsCopy = [...elements];
-       elementsCopy[selectedElement.id] = {
-         ...elementsCopy[selectedElement.id],
-         points: newPoints,
+          x: clientX - selectedElement.xOffsets[index],
+          y: clientY - selectedElement.yOffsets[index],
+        }));
+        const elementsCopy = [...elements];
+        elementsCopy[selectedElement.id] = {
+          ...elementsCopy[selectedElement.id],
+          points: newPoints,
         };
-       setElements(elementsCopy, true);
+        setElements(elementsCopy, true);
       } else {
         const { id, x1, x2, y1, y2, type, offsetX, offsetY } = selectedElement;
         const width = x2 - x1;
@@ -326,17 +322,18 @@ hbhjhj
       }
     } else if (action === "resizing") {
       const { id, type, position, ...coordinates } = selectedElement;
-      const {x1, y1, x2, y2} = resizedCoordinates(clientX, clientY, position, coordinates);
+      const { x1, y1, x2, y2 } = resizedCoordinates(clientX, clientY, position, coordinates);
       updateElement(id, x1, y1, x2, y2, type);
     }
-  }
+  };
 
   const handleMouseUp = event => {
     const { clientX, clientY } = event;
     if (selectedElement) {
-      if (selectedElement.type === "text" && 
-      clientX - selectedElement.offsetX === selectedElement.x1 &&
-      clientY - selectedElement.offsetY === selectedElement.y1
+      if (
+        selectedElement.type === "text" &&
+        clientX - selectedElement.offsetX === selectedElement.x1 &&
+        clientY - selectedElement.offsetY === selectedElement.y1
       ) {
         setAction("writing");
         return;
@@ -345,7 +342,7 @@ hbhjhj
       const index = selectedElement.id;
       const { id, type } = elements[index];
       if ((action === "drawing" || action === "resizing") && adjustmentRequired(type)) {
-        const {x1, y1, x2, y2} = adjustElementCoordinates(elements[index]);
+        const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index]);
         updateElement(id, x1, y1, x2, y2, type);
       }
     }
@@ -354,18 +351,18 @@ hbhjhj
 
     setAction("none");
     setSelectedElement(null);
-  }
+  };
 
   const handleBlur = event => {
-    const {id, x1, y1, type} = selectedElement;
+    const { id, x1, y1, type } = selectedElement;
     setAction("none");
     setSelectedElement(null);
-    updateElement(id, x1, y1, null, null, type, {text: event.target.value})
-  }
+    updateElement(id, x1, y1, null, null, type, { text: event.target.value });
+  };
 
   return (
     <div>
-      <div id="toolbar" style={{ position: "fixed" }}>
+      <div style={{ position: "fixed" }}>
         <input
           type="radio"
           id="selection"
@@ -373,12 +370,7 @@ hbhjhj
           onChange={() => setTool("selection")}
         />
         <label htmlFor="selection">Selection</label>
-        <input
-          type="radio"
-          id="line"
-          checked={tool === "line"}
-          onChange={() => setTool("line")}
-        />
+        <input type="radio" id="line" checked={tool === "line"} onChange={() => setTool("line")} />
         <label htmlFor="line">Line</label>
         <input
           type="radio"
@@ -393,49 +385,46 @@ hbhjhj
           checked={tool === "pencil"}
           onChange={() => setTool("pencil")}
         />
-        <label htmlFor="rectangle">Pencil</label>
-        <input
-          type="radio"
-          id="text"
-          checked={tool === "text"}
-          onChange={() => setTool("texxt")}
-        />
-        <label htmlFor="rectangle">Text</label>
+        <label htmlFor="pencil">Pencil</label>
+        <input type="radio" id="text" checked={tool === "text"} onChange={() => setTool("text")} />
+        <label htmlFor="text">Text</label>
       </div>
       <div style={{ position: "fixed", bottom: 0, padding: 10 }}>
         <button onClick={undo}>Undo</button>
         <button onClick={redo}>Redo</button>
       </div>
-    {action === "writing" ? (
-    <textarea 
-      ref={textAreaRef}
-      onBlur={handleBlur}
-      style={{ 
-        position: "fixed", 
-        top: selectedElement.y1 - 2, 
-        left: selectedElement.x1,
-        font: "24px sans-serif",
-        margin: 0,
-        padding: 0,
-        border: 0,
-        outline: 0,
-        resize: "auto",
-        overflow: "hidden",
-        whiteSpace: "pre",
-        background: "transparent", }} />
-    ) : null}
-    <canvas id="canvas" 
-      width={window.innerWidth} 
-      height={window.innerHeight}
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-    >
-      Canvas
-    </canvas>
+      {action === "writing" ? (
+        <textarea
+          ref={textAreaRef}
+          onBlur={handleBlur}
+          style={{
+            position: "fixed",
+            top: selectedElement.y1 - 2,
+            left: selectedElement.x1,
+            font: "24px sans-serif",
+            margin: 0,
+            padding: 0,
+            border: 0,
+            outline: 0,
+            resize: "auto",
+            overflow: "hidden",
+            whiteSpace: "pre",
+            background: "transparent",
+          }}
+        />
+      ) : null}
+      <canvas
+        id="canvas"
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        Canvas
+      </canvas>
     </div>
+  );
+};
 
-  )
-}
-
-export default IndexPage
+export default IndexPage;
